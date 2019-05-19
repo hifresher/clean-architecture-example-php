e06350e9c1c355c7d4f95210a6817fc3f08fc649
tricrow <info@tricrow.com>
2019-05-19 14:42:27 +0900

    first

--- autoloader.php
+++ autoloader.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function ($name) {
-    $path = BASE_PATH . '/' . $name . '.php';
+    $path = BASE_PATH . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $name) . '.php';
     if (is_readable($path))
         require_once $path;
 });
